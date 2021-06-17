0310eb4ed7941976957951637ca5effb159ca56a
TMarche <tmarchess@gmail.com>
2021-06-17 10:05:53 -0400

    Add image resources

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,40 @@ const App = () => {
         <nav class="nav">
             <div class="logo nav__logo">Logo</div>
             <ul class="nav-list">
-                <li class="nav-list__item">Home</li>
-                <li class="nav-list__item">Blog</li>
-                <li class="nav-list__item">About</li>
-                <li class="nav-list__item">Services</li>
-                <li class="nav-list__item">My Work</li>
-                <li class="nav-list__item">Contact</li>
+                <li class="nav-list__item"><a href="#0">Home</a></li>
+                <li class="nav-list__item"><a href="#2">About</a></li>
+                <li class="nav-list__item"><a href="#1">Blog</a></li>
+                <li class="nav-list__item"><a href="#3">Services</a></li>
+                <li class="nav-list__item"><a href="#4">My Work</a></li>
+                <li class="nav-list__item"><a href="#5">Contact</a></li>
             </ul>
         </nav>
-        <header class="header">
-            Header
+        <header class="header hero">
+            <h1 class="header__title">
+                Together, we can make it happen.
+            </h1>
+            <button class="btn header__btn">Let's Connect</button>
         </header>
+        <div class="spacer"></div>
         <div class="content">
-            Content
+            <section>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu diam, condimentum vel lacus quis, ultrices pellentesque eros. Proin a tortor pretium, congue ante eu, ultricies orci. Duis sed imperdiet nisl. Maecenas vel diam tincidunt, dapibus ligula sed, pulvinar lacus. Cras molestie consectetur nisl, eu sollicitudin elit finibus quis. Vestibulum fermentum ultrices ultricies. Nunc at nunc a elit interdum fringilla. Proin finibus neque orci, eget dictum velit bibendum et. Maecenas id sapien accumsan sapien rhoncus lobortis ac sed nisl.</p>
+            </section>
+            <section>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu diam, condimentum vel lacus quis, ultrices pellentesque eros. Proin a tortor pretium, congue ante eu, ultricies orci. Duis sed imperdiet nisl. Maecenas vel diam tincidunt, dapibus ligula sed, pulvinar lacus. Cras molestie consectetur nisl, eu sollicitudin elit finibus quis. Vestibulum fermentum ultrices ultricies. Nunc at nunc a elit interdum fringilla. Proin finibus neque orci, eget dictum velit bibendum et. Maecenas id sapien accumsan sapien rhoncus lobortis ac sed nisl.</p>
+            </section>
+            <section>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu diam, condimentum vel lacus quis, ultrices pellentesque eros. Proin a tortor pretium, congue ante eu, ultricies orci. Duis sed imperdiet nisl. Maecenas vel diam tincidunt, dapibus ligula sed, pulvinar lacus. Cras molestie consectetur nisl, eu sollicitudin elit finibus quis. Vestibulum fermentum ultrices ultricies. Nunc at nunc a elit interdum fringilla. Proin finibus neque orci, eget dictum velit bibendum et. Maecenas id sapien accumsan sapien rhoncus lobortis ac sed nisl.</p>
+            </section>
+            <section>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu diam, condimentum vel lacus quis, ultrices pellentesque eros. Proin a tortor pretium, congue ante eu, ultricies orci. Duis sed imperdiet nisl. Maecenas vel diam tincidunt, dapibus ligula sed, pulvinar lacus. Cras molestie consectetur nisl, eu sollicitudin elit finibus quis. Vestibulum fermentum ultrices ultricies. Nunc at nunc a elit interdum fringilla. Proin finibus neque orci, eget dictum velit bibendum et. Maecenas id sapien accumsan sapien rhoncus lobortis ac sed nisl.</p>
+            </section>
+            <section>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu diam, condimentum vel lacus quis, ultrices pellentesque eros. Proin a tortor pretium, congue ante eu, ultricies orci. Duis sed imperdiet nisl. Maecenas vel diam tincidunt, dapibus ligula sed, pulvinar lacus. Cras molestie consectetur nisl, eu sollicitudin elit finibus quis. Vestibulum fermentum ultrices ultricies. Nunc at nunc a elit interdum fringilla. Proin finibus neque orci, eget dictum velit bibendum et. Maecenas id sapien accumsan sapien rhoncus lobortis ac sed nisl.</p>
+            </section>
+            <section>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu diam, condimentum vel lacus quis, ultrices pellentesque eros. Proin a tortor pretium, congue ante eu, ultricies orci. Duis sed imperdiet nisl. Maecenas vel diam tincidunt, dapibus ligula sed, pulvinar lacus. Cras molestie consectetur nisl, eu sollicitudin elit finibus quis. Vestibulum fermentum ultrices ultricies. Nunc at nunc a elit interdum fringilla. Proin finibus neque orci, eget dictum velit bibendum et. Maecenas id sapien accumsan sapien rhoncus lobortis ac sed nisl.</p>
+            </section>
         </div>
         <div class="footer">
             Footer
